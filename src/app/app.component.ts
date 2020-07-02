9b61ab9cc1d3cceec028221595c0f9eebee77e97
indrajeetmukherjee <indrajeet.mukherjee@gmail.com>
2020-07-03 03:17:21 +0530

    angular design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Deal } from './deal';
 import { Facility } from './facility';
 import { Lender } from './lender';
 import { DataServiceService } from './data-service.service';
+import { Outstanding } from './outstanding';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit{
   title = 'TDF';
   topics = ['Angular','React'];
   
   
-  
+  public outstanding:Outstanding;
   public dealModel: Deal;
   public totalCommitAmount = -1
   constructor(private _dataService: DataServiceService){
+
+
+    
     //this.facilities.push(this.facilityModel);
+    this.outstanding={ostName:'',ostType:'',ostAmount:-1,
+    lender:[{lenderName:'BMO',commitAmt:1555,proRata:12,newCommitAmt:123,newProRata:13,delta:-1},{lenderName:'SCOTIA',commitAmt:1555,proRata:12,newCommitAmt:123,newProRata:13,delta:-1}]};
     this.dealModel = _dataService.getDeal()
   }
   ngOnInit (){
@@ -46,4 +53,10 @@ export class AppComponent implements OnInit{
       sum = 0
     }
   }
+  onAction(facilityIndex) {
+    console.log("Clicked Ass button")
+    this.dealModel.facility[facilityIndex].outstanding.push(this.outstanding)
+    console.log(this.dealModel.facility[facilityIndex].outstanding[0])
+  }
 }
+
--- src/app/data-service.service.ts
+++ src/app/data-service.service.ts
@@ -18,6 +18,10 @@ export class DataServiceService {
     lender:[{lenderName:'BMO',commitAmt:1555,proRata:12},{lenderName:'SCOTIA',commitAmt:1555,proRata:12}],
     outstanding:[{ostName:'OST_NAME3',ostType:'OST_TYPE3',ostAmount:14444,
     lender:[{lenderName:'BMO',commitAmt:1555,proRata:12,newCommitAmt:123,newProRata:13,delta:-1},{lenderName:'SCOTIA',commitAmt:1555,proRata:12,newCommitAmt:123,newProRata:13,delta:-1}]}]
-  }]};
+  },
+  {facilityName:'FAC_NAME2',facilityType:'FAC_TYPE2',
+  lender:[{lenderName:'BMO',commitAmt:1555,proRata:12},{lenderName:'SCOTIA',commitAmt:1555,proRata:12}],
+  outstanding:[]
+}]};
   }
 }
